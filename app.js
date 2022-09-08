6f8c54df40f979816035e4c432474b28cd3e1f80
Egidijus Jucevičius <egidijusj@wix.com>
2022-09-08 10:57:34 +0300

    Step 1

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 /* global React, ReactDOM */
 
-ReactDOM.render('Todos App', document.getElementById('app'))
+const todos = [
+  'List of Todos',
+  'Toggling a todo between active/completed',
+  'A counter for active/completed todos',
+  'Deleting a todo',
+  'Adding a new todo',
+  'Form validation'
+];
+
+const TodoApp = () => {
+  return (
+    <div>
+      <h1>Todos List</h1>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo}>
+            <h3>{todo}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(<TodoApp />, document.getElementById('app'))
